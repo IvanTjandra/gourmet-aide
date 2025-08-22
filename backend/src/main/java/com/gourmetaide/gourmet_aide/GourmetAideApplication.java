0dce5ff8795b5c4031421fe99e46641e44f8bd95
Ivan Tjandra <tjandra.ivanfernando@gmail.com>
2025-08-22 15:24:20 -0400

    chore: init repo with README and root .gitignore

---
+++ backend/src/main/java/com/gourmetaide/gourmet_aide/GourmetAideApplication.java
@@ -0,0 +1,13 @@
+package com.gourmetaide.gourmet_aide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GourmetAideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GourmetAideApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/gourmetaide/gourmet_aide/GourmetAideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gourmetaide.gourmet_aide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GourmetAideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
